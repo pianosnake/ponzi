a2d4da721da8d4eb07b2f213516ad370fb931dc1
Florin Alexandrescu <pianosnake@gmail.com>
2014-07-19 17:52:59 -0600

    First commit to Ponzi

---
+++ script.js
@@ -0,0 +1,79 @@
+(function() {
+	var User = function(props){
+		this.id =  props.id;
+		this.parent = props.parent;
+		this.level = props.level;
+		this.levelIndex = props.levelIndex;
+		this.money= 0;
+	}
+
+	var users = [];
+
+
+	var app = angular.module("ponzi", []);
+
+	app.controller("CourtCtrl", function($scope) {
+
+		$scope.clicks = 0;
+		$scope.users = users;
+		$scope.pot = 0; 
+		$scope.appCost = 5; 
+		$scope.parentShare = 1; 
+		$scope.levelCounts = [1];
+
+		var firstUser = new User(
+			{id: $scope.clicks,
+			 parent: null,
+			 level: 0,
+			 levelIndex: 0
+			});
+
+		users.push(firstUser);
+
+	
+
+		$scope.userClick = function(id) {
+			
+			$scope.clicks += 1;
+
+
+			//make new user
+			var parent = users[id];
+			var currentLevel = parent.level + 1; 
+			var currentLevelExists = (typeof $scope.levelCounts[currentLevel] != "undefined");
+
+			var newUser = new User(
+			{id: $scope.clicks ,
+			 parent: id,
+			 level: currentLevel,
+			 levelIndex: currentLevelExists ? ($scope.levelCounts[currentLevel] + 1) : 0
+			});
+
+			//distribute money
+			users[id].money += $scope.parentShare;
+			$scope.pot += ($scope.appCost - $scope.parentShare);
+			
+
+			if(currentLevelExists){
+				$scope.levelCounts[currentLevel] += 1;
+			}else{
+				$scope.levelCounts[currentLevel] = 0; 
+			}
+
+			users.push(newUser);
+console.log(users);
+		}
+
+
+		$scope.getPosition = function(id){
+			var info = users[id];
+			return{
+				left: info.levelIndex * 60 + "px",
+				top: info.level * 60 + "px"
+			}
+		}
+
+
+	})
+
+})();
